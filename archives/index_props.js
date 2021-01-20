e79808a4f91194d1847074416b0fdca87d42918b
jianyun2020 <jianyun2020@users.noreply.github.com>
2021-01-20 04:38:09 +0000

    deploy: 9bcbd47acde6be34ff4c8b66ffa30fab9bbc2aef

--- archives/index_props.js
+++ archives/index_props.js
@@ -29,83 +29,8 @@ export default {
     'cover': undefined,
     'blog': {
         "isPost": false,
-        "posts": [
-            {
-                "pagePath": "posts/没用.md",
-                "title": "为什么",
-                "link": "posts/没用.html",
-                "date": "2021-01-19T06:40:10.000Z",
-                "updated": "2021-01-19T07:25:37.000Z",
-                "author": "sujianyun",
-                "contributors": [
-                    "sujianyun",
-                    "jianyun2020"
-                ],
-                "categories": [
-                    "测试"
-                ],
-                "tags": [
-                    "pagic",
-                    "blog"
-                ],
-                "excerpt": "没有效果"
-            },
-            {
-                "pagePath": "posts/Hello World.md",
-                "title": "Blog",
-                "link": "posts/Hello World.html",
-                "date": "2021-01-19T06:40:10.000Z",
-                "updated": "2021-01-19T07:00:02.000Z",
-                "author": "sujianyun",
-                "contributors": [
-                    "sujianyun",
-                    "jianyun2020"
-                ],
-                "categories": [
-                    "测试"
-                ],
-                "tags": [
-                    "pagic",
-                    "blog"
-                ],
-                "excerpt": "测试中"
-            },
-            {
-                "pagePath": "posts/测试中.md",
-                "title": "Blog",
-                "link": "posts/测试中.html",
-                "date": "2021-01-19T06:40:10.000Z",
-                "updated": "2021-01-19T10:04:04.000Z",
-                "author": "sujianyun",
-                "contributors": [
-                    "sujianyun",
-                    "jianyun2020"
-                ],
-                "categories": [
-                    "测试"
-                ],
-                "tags": [
-                    "pagic",
-                    "blog"
-                ],
-                "excerpt": "测试中"
-            }
-        ],
-        "categories": [
-            {
-                "name": "测试",
-                "count": 3
-            }
-        ],
-        "tags": [
-            {
-                "name": "blog",
-                "count": 3
-            },
-            {
-                "name": "pagic",
-                "count": 3
-            }
-        ]
+        "posts": [],
+        "categories": [],
+        "tags": []
     }
 };
