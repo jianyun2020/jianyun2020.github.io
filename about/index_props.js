805770427146d2bf52455c4e4c701fb6705da7e0
jianyun2020 <jianyun2020@users.noreply.github.com>
2021-01-21 03:08:53 +0000

    deploy: 24f819589de0cc80ff86c48f04c109376c3c7c49

--- about/index_props.js
+++ about/index_props.js
@@ -29,8 +29,47 @@ export default {
     'cover': undefined,
     'blog': {
         "isPost": false,
-        "posts": [],
-        "categories": [],
-        "tags": []
+        "posts": [
+            {
+                "pagePath": "posts/Array.md",
+                "title": "数据结构与算法JavaScript-数组",
+                "link": "posts/Array.html",
+                "date": "2021-01-20T05:13:02.000Z",
+                "updated": "2021-01-21T03:06:45.000Z",
+                "author": "jianyun2020",
+                "contributors": [
+                    "jianyun2020"
+                ],
+                "categories": [
+                    "数据结构与算法"
+                ],
+                "tags": [
+                    "JavaScript",
+                    "数据结构与算法",
+                    "学习笔记"
+                ],
+                "excerpt": "数组 JavaScript 中的数组是一种特殊的对象， 用来表示偏移量的索引是该对象的属性， 索引可 能是整数。 然而， 这些数字索引在内部被转换为字符串类型， 这是因为 JavaScript 对象中 的属性名必须是字符串。 数组在 JavaScrip..."
+            }
+        ],
+        "categories": [
+            {
+                "name": "数据结构与算法",
+                "count": 1
+            }
+        ],
+        "tags": [
+            {
+                "name": "JavaScript",
+                "count": 1
+            },
+            {
+                "name": "学习笔记",
+                "count": 1
+            },
+            {
+                "name": "数据结构与算法",
+                "count": 1
+            }
+        ]
     }
 };
--- categories/数据结构与算法/index_props.js
+++ categories/数据结构与算法/index_props.js
@@ -1,32 +1,16 @@
 import projectConfig from '/pagic.config.js';
 export default {
     config: { "root": "/", ...projectConfig, branch: 'main' },
-    'pagePath': "archives/README.md",
+    'pagePath': "categories/数据结构与算法/",
     'layoutPath': "archives/_layout.tsx",
-    'outputPath': "archives/index.html",
-    'title': "归档",
-    'content': React.createElement("article", { dangerouslySetInnerHTML: {
-            __html: '<h1>归档</h1>'
-        } }),
-    'head': React.createElement("link", { href: "/favicon.png", rel: "icon", type: "image/png" }),
+    'outputPath': "categories/数据结构与算法/index.html",
+    'head': null,
     'script': React.createElement(React.Fragment, null,
         React.createElement("script", { src: "https://cdn.pagic.org/react@16.13.1/umd/react.production.min.js" }),
         React.createElement("script", { src: "https://cdn.pagic.org/react-dom@16.13.1/umd/react-dom.production.min.js" }),
         React.createElement("script", { src: "/index.js", type: "module" })),
-    'contentTitle': React.createElement("h1", { key: "0" }, "\u5F52\u6863"),
-    'contentBody': React.createElement("article", { dangerouslySetInnerHTML: {
-            __html: ''
-        } }),
-    'toc': null,
-    'author': "sujianyun",
-    'contributors': [
-        "sujianyun",
-        "jianyun2020"
-    ],
-    'date': "2021-01-19T04:34:29.000Z",
-    'updated': "2021-01-19T07:00:02.000Z",
-    'excerpt': "",
-    'cover': undefined,
+    'title': "数据结构与算法",
+    'content': null,
     'blog': {
         "isPost": false,
         "posts": [
