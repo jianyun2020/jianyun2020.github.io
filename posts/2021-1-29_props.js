cfae4424081a7e1c5a1abf366843d18bafb186bd
jianyun2020 <jianyun2020@users.noreply.github.com>
2021-02-02 09:57:00 +0000

    deploy: 7d6831f9e998b353d5669a024674b184838531ad

--- posts/2021-1-29_props.js
+++ posts/2021-1-29_props.js
@@ -60,7 +60,7 @@ export default {
                 "title": "React学习笔记",
                 "link": "posts/React笔记.html",
                 "date": "2021-01-29T14:58:22.000Z",
-                "updated": "2021-01-31T15:13:26.000Z",
+                "updated": "2021-02-02T09:54:38.000Z",
                 "author": "jianyun2020",
                 "contributors": [
                     "jianyun2020"
