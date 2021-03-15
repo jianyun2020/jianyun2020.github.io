d61f00d5a03323de0d723ffa4a8ecc2ee58d4235
jianyun2020 <jianyun2020@users.noreply.github.com>
2021-03-15 02:17:43 +0000

    deploy: b17044531e47bfdbfccf8ed462d0c6d62ed16c82

--- posts/微信内置浏览器播放视频_props.js
+++ posts/微信内置浏览器播放视频_props.js
@@ -42,7 +42,7 @@ export default {
                 "title": "Vuex",
                 "link": "posts/Vuex.html",
                 "date": "2021-03-15T02:10:17.000Z",
-                "updated": null,
+                "updated": "2021-03-15T02:15:28.000Z",
                 "author": "jianyun2020",
                 "contributors": [
                     "jianyun2020"
