b608d9a7b17261d1ba42d756a218551f41ffdc7a
jianyun2020 <jianyun2020@users.noreply.github.com>
2021-03-17 10:55:44 +0000

    deploy: 138fbccde2ff9af1a3df28f8d343f850600a2911

--- tags/index_props.js
+++ tags/index_props.js
@@ -35,7 +35,7 @@ export default {
                 "title": "Vue Router",
                 "link": "posts/Vue Router.html",
                 "date": "2021-03-17T08:01:47.000Z",
-                "updated": "2021-03-17T09:39:43.000Z",
+                "updated": "2021-03-17T10:53:23.000Z",
                 "author": "jianyun2020",
                 "contributors": [
                     "jianyun2020"
