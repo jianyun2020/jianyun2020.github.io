9038e4e8b6018b755728304f30f0f2d6ece175a9
jianyun2020 <jianyun2020@users.noreply.github.com>
2021-03-24 14:35:41 +0000

    deploy: 7e59f427357257ecb7fbb2b6eadf49a7252b41b3

--- posts/垃圾回收机制_props.js
+++ posts/垃圾回收机制_props.js
@@ -44,7 +44,7 @@ export default {
                 "title": "Vue",
                 "link": "posts/Vue.html",
                 "date": "2021-03-24T12:28:00.000Z",
-                "updated": "2021-03-24T14:27:28.000Z",
+                "updated": "2021-03-24T14:33:19.000Z",
                 "author": "jianyun2020",
                 "contributors": [
                     "jianyun2020"
